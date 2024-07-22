9a3ddd56b2099fa125072e1d2b7431ef4259643b
yeseul <from.yeseul@gmail.com>
2024-07-22 15:29:26 +0900

    인트로 완료

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,5 @@
+import about from "../assets/img/about.jpg";
+
 import port01 from "../assets/img/port01.jpg";
 import port02 from "../assets/img/port02.jpg";
 import port03 from "../assets/img/port03.jpg";
@@ -34,7 +36,8 @@ export const headerNav = [
 
 export const introText = {
     title: "port developer",
-    desc: ["talent is", "found at the end of the", "effort"]
+    desc: ["talent is", "found at the end of the", "effort"],
+    img: about
 }
 
 export const skillText = [
